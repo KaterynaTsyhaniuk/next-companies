983660008bd0505e2bb44340cef6cca97522c9f2
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-09-22 14:47:58 +0300

    add sidebar and settings layout

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '@/app/components/header';
+
+
+
+export default function Page() {
+    return (
+        <>
+            <Header>Dashboard</Header>
+        </>
+    );
+}
