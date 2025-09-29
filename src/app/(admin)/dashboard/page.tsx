cb4a575951fc71cb24010e3f4aeb8aad784f5f72
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2025-09-29 12:22:50 +0300

    parallel routes

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '@/app/components/header';
-import MagicButton from "@/app/components/magic-button";
+
 
 
 
@@ -8,7 +8,7 @@ export default function Page() {
     return (
         <>
             <Header>Dashboard</Header>
-            <MagicButton/>
+
         </>
     );
 }
